b3656bbcc1336da80ef37952440a96b44303c003
pyae soan aung <server01.psa1981@gmail.com>
2020-12-10 02:41:19 -0800

    dec 10 2020

---
+++ withCube/07_USB_virtual_com_port/07_usb_virtual_com_port/MDK-ARM/07_usb_virtual_com_port/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+07_usb_virtual_com_port\startup_stm32f103xe.o: startup_stm32f103xe.s
---
+++ withCube/07_USB_virtual_com_port/07_usb_virtual_com_port/MDK-ARM/RTE/_07_usb_virtual_com_port/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '07_usb_virtual_com_port' 
+ * Target:  '07_usb_virtual_com_port' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
